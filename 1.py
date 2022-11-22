af961209e53099fd02bf52e21c9f6c0167bb4512
Anton <104712265+m6129@users.noreply.github.com>
2022-11-22 20:17:17 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -5,13 +5,12 @@ from PIL import Image # библиотека для загрузки изобр�
 #from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTokenizer
 from transformers import pipeline
 
-#image_to_text = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
+image_to_text = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
 
-print("Эта модель развернута группой студентов УрФу")
 
-#@st.cache(allow_output_mutation=True)
-#def load_model():
-    #return image_to_text
+@st.cache(allow_output_mutation=True)
+def load_model():
+    return image_to_text
 
 def load_image():
     uploaded_file = st.file_uploader(label='Выберите изображение для распознования') # загрузчик файлов
@@ -25,9 +24,9 @@ def load_image():
 st.title('Классификация изображений')
 img = load_image() # вызываем функцию
 
-#result = st.button('Распознать изображение')
-#if result:
-#    x = preprocess_image(img)
-#    preds = model.predict(x)
-#    st.write('**Результаты распознавания:**')
-#    print_predictions(preds)
+result = st.button('Распознать изображение')
+if result:
+    x = preprocess_image(img)
+    preds = model.predict(x)
+    st.write('**Результаты распознавания:**')
+    print_predictions(preds)
