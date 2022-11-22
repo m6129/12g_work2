a641a05a61e93b2beb4b2816dd581c6a24700d40
Anton <104712265+m6129@users.noreply.github.com>
2022-11-22 20:22:29 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -8,7 +8,7 @@ from transformers import pipeline
 image_to_text = pipeline("image-to-text", model="nlpconnect/vit-gpt2-image-captioning")
 @st.cache(allow_output_mutation=True)
 def load_model():
-    return image_to_text
+    return image_to_text()
 
 def load_image():
     uploaded_file = st.file_uploader(label='Выберите изображение для распознования') # загрузчик файлов
